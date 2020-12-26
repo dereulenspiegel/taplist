4a415b227ad75ff1276686993e119b9703f16c3f
Till Klocke <till.klocke@gmail.com>
2020-12-26 13:44:41 +0100

    We now build correct urls for reaching the graphql endpoint. This will enable subscriptions in the future

--- frontend-src/src/main.js
+++ frontend-src/src/main.js
@@ -5,7 +5,28 @@ require('@/app.scss')
 
 Vue.config.productionTip = false
 
+var wsURL = ""
+
+if(window.location.protocol === "https:") {
+  wsURL = "wss://" + window.location.hostname
+} else {
+  wsURL = "ws://" +window.location.hostname
+}
+
+var httpURL = window.location.protocol + "//" + window.location.hostname
+
+if(window.location.port){
+  wsURL = wsURL + ":" + window.location.port
+  httpURL = httpURL + ":" + window.location.port
+}
+
+wsURL = wsURL + "/query"
+httpURL = httpURL + "/query"
+
 new Vue({
-  apolloProvider: createProvider(),
+  apolloProvider: createProvider({
+    wsEndpoint: wsURL,
+    httpEndpoint: httpURL
+  }),
   render: h => h(App)
 }).$mount('#app')
